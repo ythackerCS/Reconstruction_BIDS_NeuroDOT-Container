51bafea50fdb41c49980f69f507de7e20215cc30
ythackerCS <46691555+ythackerCS@users.noreply.github.com>
2024-04-19 14:14:42 -0500

    added files for reconstruction

---
+++ workspace/neuro_dot/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+die(){
+    echo >&2 "$@"
+    exit 1
+}
+
+echo $@
+
+SUBJECT_MAT=$1
+shift
+PARAMS=$1
+shift
+E_MAT=$1
+shift
+MNI_MAT=$1
+shift
+A_fn=$1
+shift
+MNI_path=$1
+
+dt=`date --utc +%Y%m%d-%H%M%S`
+
+echo dt 
+
+OUTPUTFOLDER_NOTEBOOK="/outputfiles/${dt}_reconstruction_notebook"
+OUTPUTFOLDER_SAVEMAT="/outputfiles/${dt}_saved_mat/"
+
+mkdir $OUTPUTFOLDER_NOTEBOOK $OUTPUTFOLDER_SAVEMAT
+
+echo 'papermill neuro_dot/NeuroDOT_Reconstruction_Script.ipynb $OUTPUTFOLDER_NOTEBOOK/output.ipynb -p patient_data /input/$SUBJECT_MAT -p params_file /input/$PARAMS -p saveMatPath $OUTPUTFOLDER_SAVEMAT'
+
+papermill neuro_dot/NeuroDOT_Reconstruction_Script.ipynb $OUTPUTFOLDER_NOTEBOOK/output.ipynb -p participant_data /input/$SUBJECT_MAT -p params_file /input/$PARAMS -p saveMatPath $OUTPUTFOLDER_SAVEMAT -p Emat $E_MAT -p MNI_file $MNI_MAT -p A_fn $A_fn --log-output --progress-bar
